b569dfc4a54e3b5594822e7bca01911bfdbf6dba
Jenny Veber <kovalsky.corp@gmail.com>
2018-05-22 14:41:19 +1000

    edit 'delete' function

--- public/js/template.js
+++ public/js/template.js
@@ -1,8 +1,9 @@
 function deleteProduct(event) {
   var xhr = new XMLHttpRequest();
-  xhr.open('post', '/list/delete', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.data = { id: event.target.id};
+  xhr.open("POST", '/list/delete', true);
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  var data = { id: event.target.id};
+  var json = JSON.stringify(data);
   xhr.onload = function() {
     if (xhr.status === 200) {
       alert('Something went wrong.  Name is now ' + xhr.responseText);
@@ -11,5 +12,6 @@ function deleteProduct(event) {
       alert('Request failed.  Returned status of ' + xhr.status);
     }
   };
-  xhr.send();
+  // xhr.send({ id: event.target.id.toString()});
+  xhr.send( json );
 }
